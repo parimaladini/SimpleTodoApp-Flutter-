3a2ec71d9699bd436ea85b352aaaec32537c6c15
parimaladni <99210041002@klu.ac.in>
2024-07-11 02:50:02 +0530

    initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color tdBlack = Color.fromARGB(255, 5, 5, 5);
+const Color tdGrey = Color.fromARGB(255, 255, 255, 255);
+
+const Color tdBGColor = Color(0xFFEEEFF5);
